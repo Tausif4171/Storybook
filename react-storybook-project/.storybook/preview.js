df1591ef0b623d7b02b1da9421fec734f3a8a5b1
Tausif4171 <tausifkhan4171@gmail.com>
2022-11-24 13:45:48 +0530

    setup chakra UI and added theming

--- react-storybook-project/.storybook/preview.js
+++ react-storybook-project/.storybook/preview.js
@@ -1,8 +1,9 @@
-import React from "react"
-import {addDecorator} from '@storybook/react'
-import Center from '../src/components/Center/Center'
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import Center from "../src/components/Center/Center";
+import { ChakraProvider } from "@chakra-ui/react";
 
-addDecorator(story =><Center>{story()}</Center>)
+addDecorator((story) => <ChakraProvider>{story()}</ChakraProvider>);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -14,6 +15,8 @@ export const parameters = {
   },
   options: {
     storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
-}
+};
